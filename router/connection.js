f4f32775e623fd099eb5ad0235816c24c6a5ae6a
Alexis Burgos <alexisburgosgtm@outlook.com>
2019-09-16 10:49:43 -0600

    correción de la cadena de conexión

--- router/connection.js
+++ router/connection.js
@@ -1,4 +1,4 @@
-const config = {user: 'DB_A45479_EXPRESS_admin',password: 'razors1805',server: 'sql7002.site4now.net',database: 'DB_A45479_EXPRESS',pool: {	max: 100,	min: 0,	idleTimeoutMillis: 30000}};
+const config = {user: 'DB_A4D278_SOLTECASA_admin',password: 'razors1805',server: 'sql5046.site4now.net',database: 'DB_A4D278_SOLTECASA',pool: {	max: 100,	min: 0,	idleTimeoutMillis: 30000}};
 //const config = {user: 'iEx', password: 'iEx', server: 'SERVERALEXIS\\SQLEXPRESS', database: 'SOLTECADB', pool: {max: 100,min: 0,idleTimeoutMillis: 30000}};
 
 let execute = {
