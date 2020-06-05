f006d3ff8ca52fc3b1e401898a39929f0b275f74
Alexis Burgos <alexisburgosgtm@outlook.com>
2020-06-04 20:10:01 -0600

    cambio de hosting a plesk

--- router/connection.js
+++ router/connection.js
@@ -1,4 +1,4 @@
-﻿const config = {user: 'DB_A4D278_SOLTECASA_admin',password: 'razors1805',server: 'sql5046.site4now.net',database: 'DB_A4D278_SOLTECASA',pool: {	max: 100,	min: 0,	idleTimeoutMillis: 30000}};
+﻿const config = {user: 'onnebusi_admin',password: 'Razors1805$',server: '65.99.252.110',database: 'onnebusi_solteca',pool: {	max: 100,	min: 0,	idleTimeoutMillis: 30000}};
 //const config = {user: 'iEx', password: 'iEx', server: 'SERVERALEXIS\\SQLEXPRESS', database: 'SOLTECADB', pool: {max: 100,min: 0,idleTimeoutMillis: 30000}};
 
 let execute = {
